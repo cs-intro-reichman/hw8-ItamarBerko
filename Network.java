c8b5ae685c6b0d2b46201f3f5d76cd499b4cefa8
Itamar Berkowitz <itamar.berkowitz@post.runi.ac.il>
2025-01-06 17:55:31 +0200

    done

--- Network.java
+++ Network.java
@@ -30,6 +30,10 @@ public class Network {
      *  Notice that the method receives a String, and returns a User object. */
     public User getUser(String name) {
         //// Replace the following statement with your code
+        for (int i = 0; i < this.userCount; i++){
+            if (name.equals(this.users[i].getName()))
+                return this.users[i];
+        }
         return null;
     }
 
@@ -39,7 +43,12 @@ public class Network {
     *  Otherwise, creates a new user with the given name, adds the user to this network, and returns true. */
     public boolean addUser(String name) {
         //// Replace the following statement with your code
-        return false;
+        if (this.getUser(name) != null || this.userCount >= this.users.length)
+            return false;
+  
+        this.users[this.userCount] = new User(name);
+        this.userCount++;
+        return true;
     }
 
     /** Makes the user with name1 follow the user with name2. If successful, returns true.
@@ -47,33 +56,71 @@ public class Network {
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
         //// Replace the following statement with your code
-        return false;
+        User user1 = this.getUser(name1);
+        User user2 = this.getUser(name2);
+        if (user1 == null || user2 == null)
+            return false;
+
+        return user1.addFollowee(name2);
     }
     
     /** For the user with the given name, recommends another user to follow. The recommended user is
      *  the user that has the maximal mutual number of followees as the user with the given name. */
     public String recommendWhoToFollow(String name) {
         //// Replace the following statement with your code
-        return null;
+        User oUser = this.getUser(name);
+        User mostRecommendedUserToFollow = null;
+        int maxMutual = 0;
+        for (int i = 0; i < this.userCount; i++){
+            if (name.equals(this.users[i].getName())) {
+                continue;
+            }
+            int mutual = oUser.countMutual(this.users[i]);
+            if (mutual > maxMutual) {
+                mostRecommendedUserToFollow = this.users[i];
+                maxMutual = mutual;
+            }
+        }
+        return mostRecommendedUserToFollow.getName();
     }
 
     /** Computes and returns the name of the most popular user in this network: 
      *  The user who appears the most in the follow lists of all the users. */
     public String mostPopularUser() {
         //// Replace the following statement with your code
-        return null;
+        User mostPoplarUser = null;
+        int maxAppearence = 0;
+        for (int i = 0; i < this.userCount; i++){
+            int appearences = this.followeeCount(this.users[i].getName());
+            if (appearences > maxAppearence){
+                mostPoplarUser = this.users[i];
+                maxAppearence = appearences;
+            }
+        }
+        return mostPoplarUser.getName();
     }
 
     /** Returns the number of times that the given name appears in the follows lists of all
      *  the users in this network. Note: A name can appear 0 or 1 times in each list. */
     private int followeeCount(String name) {
         //// Replace the following statement with your code
-        return 0;
+        int count = 0;
+        for (int i = 0; i < this.userCount; i++){
+            if (this.users[i].follows(name)) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
        //// Replace the following statement with your code
-       return null;
+       String ans = "Network:";
+       for (int i = 0; i < userCount; i++) {
+           ans += "\n" + users[i];
+       }
+       return ans;
     }
 }
